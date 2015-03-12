180377202f9b49ecdf79441d3124af7d65775ade
skoam <delatio.s@gmail.com>
2015-03-12 11:35:37 +0100

    Code structuring

--- Classes/gameobject.py
+++ Classes/gameobject.py
@@ -5,18 +5,26 @@ from common import random_id, Position, Size
 
 class GameObject:
     def __init__(self, name=None, position=None, size=None, image=None):
-        self.name = name if name is not None else "unnamed game object " + random_id()
-        self.position = position if position is not None else Position(0, 0)
-        self.size = size if size is not None else Size(0, 0)
+        default_name = "gameobject-" + random_id()
+        
+        self.name = name if name is not None else default_name
 
-        if not image:
-            self.image = images.get_image(images.resources["empty"])
-        else:
-            self.image = image
+        def sizeAndPosition():
+          self.position = position if position is not None else Position(0, 0)
+          self.size = size if size is not None else Size(0, 0)
 
-        self.image = pygame.transform.scale(self.image, (self.size.width, self.size.height))
-        self.rect = pygame.Rect(self.position.x, self.position.y, self.size.width, self.size.height)
-        self.color = False
+        def define_image():
+          if not image:
+              self.image = images.get_image(images.resources["empty"])
+          else:
+              self.image = image
+
+          self.image = pygame.transform.scale(self.image, (self.size.width, self.size.height))
+          self.rect = pygame.Rect(self.position.x, self.position.y, self.size.width, self.size.height)
+          self.color = False
+
+        sizeAndPosition()
+        define_image()
 
     def chimg(self, img):
         self.image = images.get_image(images.resources[img])
